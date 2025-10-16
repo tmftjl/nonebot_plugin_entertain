90e19255291971b9f22bc963f7ded7211dedca2b
dggb <>
2025-10-16 23:34:21 +0800

    1

--- plugins/entertain/musicshare.py
+++ plugins/entertain/musicshare.py
@@ -62,7 +62,7 @@ USER_RESULTS: Dict[str, Tuple[Platform, List[Song]]] = {}
 
 
 # Regex definitions
-P = Plugin(name="musicshare", display_name="点歌")
+P = Plugin(name="entertain", display_name="娱乐")
 search_matcher = P.on_regex(
     r"^#?点歌(?:(qq|酷狗|网易云|wyy|kugou|netease))?\s*(.*)$",
     name="search",
